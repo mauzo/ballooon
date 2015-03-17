78733d9ff4f1dec0f02e156ed19d86c3deb4ad11
Ben Morrow <ben@morrow.me.uk>
2015-03-17 13:33:53 +0000

    Experiments with strings in flash.
    Unfortunately I can't make any of this work usefully in C++: it doesn't
    understand the __flash storage qualifier that makes the C compiler
    generate LPM instructions, and it's stricter about types in a way that's
    really unhelpful when working at this level.
    
    [I'd really like to ditch the Arduino C++ libraries altogether and do
    the whole thing in C. I don't think it would be *that* much work: the
    Wire library already just calls a C library called twi that does all the
    actual work; the serial driver is the simplest thing in the world; and I
    think that just leaves a VFAT driver, which I should be able to lift
    from BSD or wherever... Hmm...]

---
+++ flash.c
@@ -0,0 +1,51 @@
+typedef struct pstr {
+    unsigned char   len;
+    char            dat[1];
+} pstr;
+typedef const __flash pstr fstr;
+
+#define F(s) ({ \
+    static const __flash struct { \
+        unsigned char len; \
+        char dat[sizeof(s) - 1]; \
+    } __Fstr = { sizeof(s), (s) }; \
+    (fstr *)&__Fstr; \
+})
+#define dFSTR(n, s) \
+    const __flash struct { \
+        unsigned char len; \
+        char dat[sizeof(s) - 1]; \
+    } n  = { sizeof(s), (s) }
+#define dFBYTES(n, ...) \
+    const __flash struct { \
+        unsigned char len; \
+        unsigned char dat[sizeof((unsigned char[]){__VA_ARGS__})]; \
+    } n = { sizeof((unsigned char[]){__VA_ARGS__}), __VA_ARGS__ }
+
+static char buf[81];
+
+static dFSTR(toobig, "Flash string too big");
+static dFBYTES(setIOtoUBX, 0x62, 0xB5, 0x01, 0x00);
+
+void panic(fstr *msg);
+
+unsigned char
+fstrdup(fstr *s)
+{
+    unsigned char l, i;
+
+    l = s->len;
+    if (l > sizeof buf)
+        panic(&toobig);
+    for (i = 0; i < l; i++)
+        buf[i] = s->dat[i];
+
+    return l;
+}
+
+void
+foo (void)
+{
+    fstrdup(&setIOtoUBX);
+    panic(F("foo"));
+}
