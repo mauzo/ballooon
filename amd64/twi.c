6a32bebcfc2dc20312b526ea4761d839e1a604c0
Ben Morrow <ben@morrow.me.uk>
2015-03-27 16:05:22 +0000

    Send the amd64 twi debugging to stderr.

--- amd64/twi.c
+++ amd64/twi.c
@@ -35,21 +35,22 @@ twi_readFrom (uint8_t addr, uint8_t *data, uint8_t len, uint8_t stop)
     static struct reply *r = replies;
     byte l = len;
 
-    printf("Call to twi_readFrom addr [%x] len [%u] stop [%s]\n",
+    fprintf(stderr, "Call to twi_readFrom addr [%x] len [%u] stop [%s]\n",
         addr, len, (stop?"yes":"no"));
 
     if (!r->dat) {
-        printf("  Nothing to return!\n");
+        fprintf(stderr, "  Nothing to return!\n");
         exit(1);
     }
 
+    fprintf(stderr, "  returning:");
     while (l && r->len) {
         *data = *(r->dat);
-        printf(" %02x", *data);
+        fprintf(stderr, " %02x", *data);
         data++; r->dat++;
         l--;    r->len--;
     }
-    printf("\n  returned [%u] bytes\n", len - l);
+    fprintf(stderr, "\n  returned [%u] bytes\n", len - l);
 
     if (!r->len) r++;
 
@@ -60,15 +61,16 @@ uint8_t
 twi_writeTo (uint8_t addr, uint8_t *data, uint8_t len,
     uint8_t wait, uint8_t stop)
 {
-    printf("Call to twi_writeTo addr [%x] len [%u] wait [%s] stop [%s]\n"
+    fprintf(stderr,
+        "Call to twi_writeTo addr [%x] len [%u] wait [%s] stop [%s]\n"
         "  data:",
         addr, len, (stop?"yes":"no"), (wait?"yes":"no"));
     while (len) {
-        printf(" %02x", *data);
+        fprintf(stderr, " %02x", *data);
         data++;
         len--;
     }
-    printf("\n");
+    fprintf(stderr, "\n");
 
     return 0;
 }
